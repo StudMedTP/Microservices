bf54339252e013cfcd48f00e2a0cb0282385818a
JoseR200 <joserodrigolopez@icloud.com>
2026-05-17 11:49:02 +0200

    Update CreateClinicHistoryResource.java

--- evaluation/src/main/java/com/studmed/evaluation/clinichistory/interfaces/rest/resource/CreateClinicHistoryResource.java
+++ evaluation/src/main/java/com/studmed/evaluation/clinichistory/interfaces/rest/resource/CreateClinicHistoryResource.java
@@ -3,6 +3,7 @@ package com.studmed.evaluation.clinichistory.interfaces.rest.resource;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record CreateClinicHistoryResource(
         @NotBlank
@@ -11,6 +12,7 @@ public record CreateClinicHistoryResource(
         @Min(1)
         Integer age,
         @NotBlank
+        @Pattern(regexp = "[MF]", message = "El sexo solo puede ser 'M' o 'F'")
         String sex,
         @NotBlank
         String mainDiagnosis,
