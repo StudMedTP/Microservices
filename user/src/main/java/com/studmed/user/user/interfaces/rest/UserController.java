5a3c95fb5f27404a9cf384fe1d29eb936fb7f30c
JoseR200 <joserodrigolopez@icloud.com>
2026-05-26 12:20:13 +0200

    Update UserController.java

--- user/src/main/java/com/studmed/user/user/interfaces/rest/UserController.java
+++ user/src/main/java/com/studmed/user/user/interfaces/rest/UserController.java
@@ -45,7 +45,7 @@ public class UserController {
 
     @GetMapping("/ping")
     public ResponseEntity<String> ping() {
-        return ResponseEntity.ok("User Microservice is up and running! 2.0");
+        return ResponseEntity.ok("User Microservice is up and running! 2.1");
     }
 
     @PostMapping
